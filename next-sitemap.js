0355ec723420c9882c4089dde019b8d9b0746d02
TrueSoulCoder <chrisphillips@truesoulpartners.com>
2025-06-16 05:16:17 -0500

    changed site url mapping for sitemap generation

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
 // next-sitemap.js
 const sitemapConfig = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000',
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'http://truesoulpartners.vercel.app',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }]
